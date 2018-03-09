1d75225aef55d86efa043834c8516ae87f0c1b52
ADMIN <ADMIN@DESKTOP-1L0PIIL>
2018-03-09 11:56:33 +0530

    likes

--- src/main/java/com/niit/dao/NotificationDao.java
+++ src/main/java/com/niit/dao/NotificationDao.java
@@ -1,11 +1,53 @@
 package com.niit.dao;
 
+
+
 import java.util.List;
+
+
 
 import com.niit.model.Notification;
 
-public interface NotificationDao {
-	List<Notification> getNotificationNotViewed(String email);
-	
 
-}
+
+public interface NotificationDao {
+
+	List<Notification> getNotificationNotViewed(String email);
+
+
+
+	Notification getNotification(int id);
+
+
+
+	void updateNotification(int id);
+
+
+
+}/*package com.niit.dao;
+
+
+
+import java.util.List;
+
+
+
+import com.niit.model.Notification;
+
+
+
+public interface NotificationDao {
+
+	List<Notification> getNotificationNotViewed(String email);
+
+
+
+	Notification getNotification(int id);
+
+
+
+	void updateNotification(int id);
+
+
+
+}*/
--- src/main/java/com/niit/model/Notification.java
+++ src/main/java/com/niit/model/Notification.java
@@ -16,6 +16,91 @@ import javax.persistence.Table;
 
 @Entity
 
+@Table(name="notification_table1")
+
+public class Notification {
+
+	@Id
+
+	@GeneratedValue(strategy=GenerationType.AUTO)
+
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getBlogTitle() {
+		return blogTitle;
+	}
+
+	public void setBlogTitle(String blogTitle) {
+		this.blogTitle = blogTitle;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getApprovalStatus() {
+		return approvalStatus;
+	}
+
+	public void setApprovalStatus(String approvalStatus) {
+		this.approvalStatus = approvalStatus;
+	}
+
+	public String getRejectionReason() {
+		return rejectionReason;
+	}
+
+	public void setRejectionReason(String rejectionReason) {
+		this.rejectionReason = rejectionReason;
+	}
+
+	public boolean isViewed() {
+		return viewed;
+	}
+
+	public void setViewed(boolean viewed) {
+		this.viewed = viewed;
+	}
+
+	private String blogTitle;
+
+	private String email;
+
+	private String approvalStatus;
+
+	private String rejectionReason;
+
+	private boolean viewed;}
+/*package com.niit.model;
+
+
+
+import javax.persistence.Entity;
+
+import javax.persistence.GeneratedValue;
+
+import javax.persistence.GenerationType;
+
+import javax.persistence.Id;
+
+import javax.persistence.Table;
+
+
+
+@Entity
+
 @Table(name="notification_table")
 
 public class Notification {
@@ -83,4 +168,4 @@ public class Notification {
 	public void setViewed(boolean viewed) {
 		this.viewed = viewed;
 	}
-}
+}*/
